c5b343ac1a16a34853f273de87b9a8bbbdadba6c
data-miner00 <data-miner00@users.noreply.github.com>
2021-06-17 22:14:06 +0800

    feat: Add confirm interface base on analysis

---
+++ src/app/interfaces/Confirmed.ts
@@ -0,0 +1,20 @@
+export default interface Confirmed {
+  provinceState?: string;
+  countryRegion: string;
+  lastUpdate: bigint;
+  lat?: number;
+  long?: number;
+  confirmed: number;
+  recovered?: number;
+  deaths: number;
+  active?: number;
+  admin2?: string;
+  fips?: number;
+  combinedKey: string;
+  incidentRate?: number;
+  peopleTested?: number;
+  peopleHospitalized?: number;
+  uid: number;
+  iso3?: string;
+  iso2?: string;
+}
