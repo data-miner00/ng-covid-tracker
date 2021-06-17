05546cf65ec8914f08797dd9c08143e52580b6d3
data-miner00 <data-miner00@users.noreply.github.com>
2021-06-17 22:13:36 +0800

    feat: Added analysis for confirmed endpoint

---
+++ analysis/confirmed.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+df = pd.read_json('./data/confirmed.json')
+
+# Describe the data
+df.describe()
+
+# Show column names
+df.columns
+
+# 
+df['admin2'].isna().sum()
+
+df['admin2'].unique()
+
+df['admin2'].unique().size
+
+# Calculate null value based on columns
+df.isna().sum(axis=0)
+
+# 
+df['fips'].dtype
+
+# Show all datatypes for the columns
+df.dtypes
