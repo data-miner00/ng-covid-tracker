79091d0b7989885e5359f9b2e25894e6b3641037
data-miner00 <data-miner00@users.noreply.github.com>
2021-06-19 23:03:40 +0800

    feat: Better header implementation

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.sass']
+  styleUrls: ['./header.component.sass'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    const burgerIcon: HTMLElement = document.querySelector('#burger');
+    const navbarMenu: HTMLElement = document.querySelector('#nav-links');
+    burgerIcon.addEventListener('click', () => {
+      navbarMenu.classList.toggle('is-active');
+    });
   }
-
 }
