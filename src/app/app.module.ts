cd3f85eb9c880dd4259ea6c18094184552a45c1e
data-miner00 <data-miner00@users.noreply.github.com>
2021-06-19 23:05:10 +0800

    feat: Registered echarts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+import { NgxEchartsModule } from 'ngx-echarts';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
@@ -9,7 +11,14 @@ import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NgxEchartsModule.forRoot({
+      echarts: () => import('echarts'),
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
