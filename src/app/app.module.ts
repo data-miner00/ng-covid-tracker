ec5ba31252da36bca7dd8957f943cdd17eb43cd8
data-miner00 <data-miner00@users.noreply.github.com>
2021-06-20 17:22:55 +0800

    chore: Register echarts components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,34 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { NgxEchartsModule } from 'ngx-echarts';
+import * as echarts from 'echarts/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { StatCardComponent } from './components/stat-card/stat-card.component';
+
+import { BarChart, LineChart } from 'echarts/charts';
+
+import {
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  LegendComponent,
+} from 'echarts/components';
+
+import { CanvasRenderer } from 'echarts/renderers';
+
+echarts.use([
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  BarChart,
+  LineChart,
+  CanvasRenderer,
+  LegendComponent,
+]);
 
 @NgModule({
   declarations: [
@@ -22,7 +44,7 @@ import { StatCardComponent } from './components/stat-card/stat-card.component';
     AppRoutingModule,
     HttpClientModule,
     NgxEchartsModule.forRoot({
-      echarts: () => import('echarts'),
+      echarts,
     }),
   ],
   providers: [],
