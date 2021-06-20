3480ce9e1e6b8372dc7b547f7cc1f2f1a189c2f9
data-miner00 <data-miner00@users.noreply.github.com>
2021-06-20 23:01:31 +0800

    feat: Updates to home script

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -4,6 +4,9 @@ import { CovidApiService } from 'src/app/service/covid-api.service';
 import { EChartsOption } from 'echarts';
 import globalStatsBar from 'src/app/charts/global-stats-bar';
 import globalSvenDaysAvgLine from 'src/app/charts/global-sven-days-avg-line';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { IconDefinition } from '@fortawesome/fontawesome-common-types';
+import countryNames from 'src/data/country-array.json';
 
 @Component({
   selector: 'app-home',
@@ -14,11 +17,13 @@ export class HomeComponent implements OnInit {
   confirmed: Confirmed[] = [];
   chartOption: EChartsOption;
   globalLineChart: EChartsOption;
+  faCheckCircle: IconDefinition = faCheckCircle;
+  countries: string[] = countryNames;
 
   constructor(private covidApiService: CovidApiService) {}
 
   ngOnInit(): void {
     this.chartOption = globalStatsBar(178673297, 163197981, 3868612);
-    this.globalLineChart = globalSvenDaysAvgLine(['']);
+    this.globalLineChart = globalSvenDaysAvgLine([''], [], []);
   }
 }
