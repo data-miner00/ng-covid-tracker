bf828e240353e76a8ff84622713ede64aa3c703c
data-miner00 <data-miner00@users.noreply.github.com>
2021-06-22 14:45:40 +0800

    feat: Registered more routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './views/home/home.component';
+import { InfoComponent } from './views/info/info.component';
+import { AboutComponent } from './views/about/about.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'info',
+    component: InfoComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
   },
 ];
 
