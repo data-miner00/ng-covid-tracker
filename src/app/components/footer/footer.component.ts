2372703943d4d8147f6e121f79f798a1c9312bd8
data-miner00 <data-miner00@users.noreply.github.com>
2021-06-22 15:03:08 +0800

    feat: Added footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { faSnowflake } from '@fortawesome/free-solid-svg-icons';
+import { IconDefinition } from '@fortawesome/fontawesome-common-types';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.sass']
+  styleUrls: ['./footer.component.sass'],
 })
 export class FooterComponent implements OnInit {
+  faSnowflake: IconDefinition = faSnowflake;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
