25b032030ee6cd1a095e711360187da0e1da7ceb
data-miner00 <data-miner00@users.noreply.github.com>
2021-06-23 19:43:43 +0800

    feat: Added more utility functions

--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -1,3 +1,17 @@
+import * as moment from 'moment';
+
 export function numberWithCommas(x: number): string {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+export const calculateDelta = (current: number, previous: number): string => {
+  return (((current - previous) / previous) * 100).toFixed(2);
+};
+
+export function getFormattedDateForAPI(dayOffset: number): string {
+  return moment().subtract(dayOffset, 'days').startOf('day').format('M-D-YYYY');
+}
+
+export function getFormattedDateForDisplay(date: string): string {
+  return moment(date).calendar();
 }
