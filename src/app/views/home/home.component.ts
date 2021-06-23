db227fa32524e222e29931aa8d488fdd7ae38e88
data-miner00 <data-miner00@users.noreply.github.com>
2021-06-23 19:44:53 +0800

    bug: Bugged homepage for fix

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -8,6 +8,11 @@ import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { IconDefinition } from '@fortawesome/fontawesome-common-types';
 import countryNames from 'src/data/country-array.json';
 import countryDistPie from 'src/app/charts/country-dist-pie';
+import {
+  getFormattedDateForDisplay,
+  getFormattedDateForAPI,
+  calculateDelta,
+} from 'src/app/utils';
 
 @Component({
   selector: 'app-home',
@@ -15,6 +20,23 @@ import countryDistPie from 'src/app/charts/country-dist-pie';
   styleUrls: ['./home.component.sass'],
 })
 export class HomeComponent implements OnInit {
+  globalConfirmed: number;
+  globalRecovered: number;
+  globalDeaths: number;
+  globalLastUpdate: string;
+
+  globalConfirmedYesterday: number = 0;
+  globalRecoveredYesterday: number = 0;
+  globalDeathsYesterday: number = 0;
+
+  globalNewConfirmed: number;
+  globalNewRecovered: number;
+  globalNewDeaths: number;
+
+  globalNewConfirmedDelta: string;
+  globalNewRecoveredDelta: string;
+  globalNewDeathsDelta: string;
+
   chartOption: EChartsOption;
   globalLineChart: EChartsOption;
   countryDistChart: EChartsOption;
@@ -27,7 +49,53 @@ export class HomeComponent implements OnInit {
   constructor(private covidApiService: CovidApiService) {}
 
   ngOnInit(): void {
-    this.chartOption = globalStatsBar(178673297, 163197981, 3868612);
+    // Async calls needs to be nested
+    this.covidApiService.getGeneralInfo().subscribe((general) => {
+      this.globalConfirmed = general.confirmed.value;
+      this.globalRecovered = general.recovered.value;
+      this.globalDeaths = general.deaths.value;
+      this.globalLastUpdate = getFormattedDateForDisplay(general.lastUpdate);
+
+      this.covidApiService
+        .getDailyAccordingDate(getFormattedDateForAPI(1))
+        .subscribe((dailys) => {
+          dailys.forEach((daily) => {
+            console.log(daily.confirmed);
+            this.globalConfirmedYesterday += Number(daily.confirmed);
+            this.globalRecoveredYesterday += Number(daily.recovered);
+            this.globalDeathsYesterday += Number(daily.deaths);
+          });
+
+          this.globalNewConfirmed =
+            this.globalConfirmed - this.globalConfirmedYesterday;
+          this.globalNewRecovered =
+            this.globalRecovered - this.globalRecoveredYesterday;
+          this.globalNewDeaths = this.globalDeaths - this.globalDeathsYesterday;
+
+          this.globalNewConfirmedDelta = calculateDelta(
+            this.globalConfirmed,
+            this.globalConfirmedYesterday
+          );
+          this.globalNewRecoveredDelta = calculateDelta(
+            this.globalRecovered,
+            this.globalRecoveredYesterday
+          );
+          this.globalNewDeathsDelta = calculateDelta(
+            this.globalDeaths,
+            this.globalDeathsYesterday
+          );
+          console.log(this.globalConfirmedYesterday);
+          console.log(this.globalNewConfirmed);
+          console.log(this.globalNewConfirmedDelta);
+
+          this.chartOption = globalStatsBar(
+            this.globalConfirmed,
+            this.globalRecovered,
+            this.globalDeaths
+          );
+        });
+    });
+
     this.globalLineChart = globalSvenDaysAvgLine([''], [], []);
     this.countryDistChart = countryDistPie(
       this.selectedCountry,
